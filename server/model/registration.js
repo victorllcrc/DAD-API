afaabee40f7ac326003ebe4fc18c43b5045754d0
unknown <victor.llancari@unmsm.edu.pe>
2024-06-04 00:02:19 -0500

    feat

--- server/model/registration.js
+++ server/model/registration.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const registrationSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     course_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+    status:{
+        type: String,
+        required: true,
+        default: 'not-started'
+    },
 })
 
 const Registration = mongoose.model('Registration', registrationSchema);
